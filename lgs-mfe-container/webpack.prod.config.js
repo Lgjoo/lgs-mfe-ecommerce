d28d1eb467432eda002a7b32cca5e243b858d3d8
Luiz Guilherme SIlva <lgsilva.lgs19@gmail.com>
2025-05-12 12:22:05 -0300

    feat: apply changes to deploy script

--- lgs-mfe-container/webpack.prod.config.js
+++ lgs-mfe-container/webpack.prod.config.js
@@ -1,1 +1,14 @@
-module.exports = require('./webpack.config');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "lgs-mfe-catalog": "lgs-mfe-catalog@https://lgs-mfe-catalog.onrender.com/remoteEntry.js",
+    "lgs-mfe-cart": "lgs-mfe-cart@https://lgs-mfe-cart.onrender.com/remoteEntry.js"   
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
